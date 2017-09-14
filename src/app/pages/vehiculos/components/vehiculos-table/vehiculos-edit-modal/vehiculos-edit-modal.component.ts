a9a810dc7e4ebb25ebae7ecffb9de57ef351db26
cesardesarrollo <cesar_alonso_m_g@hotmail.com>
2017-09-14 13:44:08 -0500

    vehiculos

---
+++ src/app/pages/vehiculos/components/vehiculos-table/vehiculos-edit-modal/vehiculos-edit-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-vehiculo-edit-modal',
+    templateUrl: './vehiculo-edit-modal.component.html'
+})
+
+export class VehiculoEditModalComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
---
+++ src/app/pages/vehiculos/components/vehiculos-table/vehiculos-response.interface.ts
@@ -0,0 +1,23 @@
+export interface VehiculosResponseInterface {
+    status?: string;
+    message?: string;
+    data: {
+        idvehiculo?: number;
+        marca: string;
+        modelo: string;
+        anio: number;
+        serie: string;
+        placas: string;
+        descripcion: string;
+        condicion_inicial: string;
+        condicion_actual: string;
+        estaus_actividad: string;
+        baja: boolean;
+        propietario_idpropietario: number;
+        permiso_idpermiso: number;
+        fecha_asigancion_permiso: string;
+        chofer_idchofer: number;
+        created_at: string;
+        created_by: string;
+    }
+} 
